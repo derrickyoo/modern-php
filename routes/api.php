eaf3b0b2f98845515a05d52c397f6539b134345d
Derrick Yoo <derrickyoo@gmail.com>
2024-07-21 21:14:38 -0700

    Config user resource

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
@@ -10,11 +9,6 @@ Route::get('/', function () {
     ], 200);
 });
 
-
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
